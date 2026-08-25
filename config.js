575e2aef183fed1185126c9d23f88b6e13a4931e
edurugby8 <edubabarro8.5@gmail.com>
2026-08-25 23:25:13 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+/* Configuración pública de NORTEPACK. No añadas aquí claves secretas. */
+window.NORTEPACK_CONFIG = Object.freeze({
+  supabaseUrl: "https://orqujsouowncwxsqonuh.supabase.co",
+  supabasePublishableKey: "sb_publishable_ZU3HLfpZTP3kLK3CUktG4g_SmDu9Ny-",
+  functionsUrl: "https://orqujsouowncwxsqonuh.supabase.co/functions/v1/collaborations",
+  siteUrl: "https://www.nortepack.es"
+});
